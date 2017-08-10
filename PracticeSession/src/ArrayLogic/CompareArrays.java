1df4e12f958037f46f23b6829d9ef90bff5cfde8
venkat <venkat@full366-prt.full.creative>
2017-08-10 17:10:41 +0530

    "Update for Array validation

--- PracticeSession/src/ArrayLogic/CompareArrays.java
+++ PracticeSession/src/ArrayLogic/CompareArrays.java
@@ -7,8 +7,31 @@ public class CompareArrays {
 	public static void main(String[] args) {
 
 		int arr[]={1,2,3,4,5,6};
-		int arr1[]={1,2,3,4,5,6};
+		int arr1[]={1,2,4,5,6,3};
 		System.out.println(Arrays.equals(arr, arr1));
+		
+		
+		// another way of comparing 
+		int count=0;
+		int a=arr.length;
+		int b=arr1.length;
+		if(a == b){
+			for(int i=0;i<a;i++){
+				for(int j=0;j<b;j++){
+					if(i==j && arr[i] == arr1[j] ){
+						count++;
+					}else{
+						continue;
+					}
+				}
+			}
+			
+			if(count == a){
+				System.out.println("Both are same");
+			}
+			else System.out.println("===========Both are not same");
+		}else
+			System.out.println("Both are not same==========");
 	}
 
 }
